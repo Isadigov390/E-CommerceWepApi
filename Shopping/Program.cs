71f57777ba84295d525b600dbe4b5ca5aa8b3e2f
sadiqov2005 <isays@code.edu.az>
2026-07-21 10:02:12 +0400

    Fix: CORS middleware order

--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -58,11 +58,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
+};
 app.UseHttpsRedirection();
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
+
 
 app.MapControllers();
 
